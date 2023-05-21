0968bc262f7abb2249ce8865af4b00530d8b8713
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-05-21 10:41:43 +0900

    Response Model and Error Handling

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class TodoItem(BaseModel):
@@ -8,6 +9,23 @@ class TodoItem(BaseModel):
         schema_extra = {
             "example": {
                 "item": "Read the next chapter of the book"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "exmaple": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
             }
         }
 
